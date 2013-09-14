938482f6bf21196c0f4ac688da4717cba7c1fd9c
Anubhaw Arya <anubhaw.arya@gmail.com>
2013-09-13 23:26:50 -0700

    Starting UI Crap

---
+++ src/com/flapjack/FlapJackAndroid/Input.java
@@ -0,0 +1,16 @@
+package com.flapjack.FlapJackAndroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * User: AnubhawArya
+ * Date: 9/13/13
+ * Time: 10:59 PM
+ */
+public class Input extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Input);
+    }
+}
